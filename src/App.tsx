d670b2d1f95d033106d4264d838e9d23a5f67652
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-08-08 16:31:01 -0500

    rutas completas

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from "./components/home";
+import Dashboard from "./components/dashboard";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Mi despensa</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/config/config.ts
@@ -0,0 +1,12 @@
+export const config = {
+  // DO NOT USE THESE CREDENTIALS ! THEY ARE HERE TO HELP IN THE LEARNING PROCESS.
+  // ANY AND ALL DATA ON THAT DOMAIN IS SUBJECT TO CHANGE AND REMOVAL AT ANY TIME
+  // THIS ACCOUNT IS ALSO ON THE FREE PLAN AND IS SUBJECT TO RESTRICTIONS !
+  databaseURL: "https://mi-despensa-7a229.firebaseio.com",
+  apiKey: "AIzaSyD94iISO20zUX6uMA_xneapYKbgPyQH4y8",
+  authDomain: "mi-despensa-7a229.firebaseapp.com",
+  projectId: "mi-despensa-7a229",
+  storageBucket: "mi-despensa-7a229.appspot.com",
+  messagingSenderId: "800758036960",
+  appId: "1:800758036960:web:11d2eedc6a5fa33fe5d385",
+};

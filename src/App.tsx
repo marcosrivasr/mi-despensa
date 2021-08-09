3f1a786538800d8a35d37c0f8cbccbbbac002ffd
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-08-08 22:27:32 -0500

    rutas agregadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,42 @@
 import Home from "./components/home";
 import Dashboard from "./components/dashboard";
-import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from "./components/login";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import { FirebaseAuthProvider } from "@react-firebase/auth";
+import { config } from "./config/config";
+import Header from "./components/header";
+import { headerDataItems } from "./data/headerItems";
+import Logout from "./components/logout";
+import {
+  FirestoreProvider,
+  FirestoreDocument,
+} from "@react-firebase/firestore";
 
 export const App = () => {
   return (
     <div>
-      <h1>Mi despensa</h1>
-
-      <BrowserRouter>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/home">
-            <Home />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <FirebaseAuthProvider {...config} firebase={firebase}>
+        <BrowserRouter>
+          <Header {...headerDataItems} />
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/logout">
+              <Logout />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </FirebaseAuthProvider>
     </div>
   );
 };

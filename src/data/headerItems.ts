a317e956de5726d88592073a8e38a4d09e33ca84
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-08-08 22:29:38 -0500

    items para header

---
+++ src/data/headerItems.ts
@@ -0,0 +1,29 @@
+import { HeaderProps } from "../types/header";
+import firebase from "firebase/app";
+import "firebase/auth";
+import { Link } from "react-router-dom";
+
+export const headerDataItems: HeaderProps = {
+  items: [
+    {
+      id: "0",
+      title: "Home",
+      goTo: "/home",
+    },
+    {
+      id: "1",
+      title: "Sign in",
+      goTo: "/login",
+    },
+    {
+      id: "3",
+      title: "Dashboard",
+      goTo: "/dashboard",
+    },
+    {
+      id: "2",
+      title: "Log out",
+      goTo: "/logout",
+    },
+  ],
+};

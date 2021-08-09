c321078c4cf41180fab6590e06af0ae3b533b7c4
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-08-08 22:28:40 -0500

    creación de home

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,10 +1,25 @@
 import { Link } from "react-router-dom";
+import "firebase/auth";
+import { IfFirebaseAuthedAnd } from "@react-firebase/auth";
 export default function Home() {
   return (
     <div>
       <h2>Home</h2>
       <Link to="/home">Home</Link> {}
       <Link to="/dashboard">Dashboard</Link>
+      <div>
+        <IfFirebaseAuthedAnd
+          filter={({ providerId }) => providerId !== "anonymous"}
+        >
+          {({ providerId, user }) => {
+            return (
+              <div>
+                You are authenticated with {providerId} {user.displayName}
+              </div>
+            );
+          }}
+        </IfFirebaseAuthedAnd>
+      </div>
     </div>
   );
 }

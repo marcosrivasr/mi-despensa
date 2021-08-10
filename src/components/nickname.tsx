cb1190c966cbf7eb5eb790d032e13d976978e65c
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-08-10 01:37:09 -0500

    rediseño de login para terminar proceso

--- src/components/nickname.tsx
+++ src/components/nickname.tsx
@@ -1,71 +1,102 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "firebase/auth";
 import "firebase/firestore";
 import firebase from "firebase/app";
-import { DataState } from "../types/dataState";
+import { LoginState } from "../types/dataState";
 import { Redirect } from "react-router";
 
 export default function Nickname() {
-  const [username, setUsername] = useState<string>(null);
-  const [user, setUser] = useState<firebase.User>(null);
-  const [isUserSigned, setIsUserSigned] = useState<DataState>(DataState.None);
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [loginProcess, setLoginProcess] = useState(LoginState.None);
 
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-      setIsUserSigned(DataState.Loading);
-      setUser(user);
-      // ...
-    } else {
-      setIsUserSigned(DataState.Error);
-      // User is signed out
-      // ...
-    }
-  });
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log("hay usuario");
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        //setIsUserSigned(DataState.Loading);
+        setUser(user);
+        // ...
+      } else {
+        //setIsUserSigned(DataState.Error);
+      }
+    });
+  }, []);
 
   async function handleSubmit(e) {
     e.preventDefault();
 
-    if (username || username.trim() !== "") {
-      const { uid, displayName, email, photoURL } = user;
-      const response = firebase.firestore().collection("users").doc(uid).set({
-        username,
-        uid,
-        displayName,
-        email,
-        photoURL,
-      });
+    if (!username || username.trim() === "") {
+      return;
+    }
 
-      setIsUserSigned(DataState.Completed);
+    const usernameExists = await firebase
+      .firestore()
+      .collection("users")
+      .where("username", "==", username)
+      .get();
+
+    if (usernameExists.empty) {
+      setLoginProcess(LoginState.UsernameAvailable);
+    } else {
+      setLoginProcess(LoginState.UsernameTaken);
     }
   }
 
-  function handleChange(e) {
-    setUsername(e.target.value);
+  async function handleCompleteProcess() {
+    const { uid, displayName, email, photoURL } = user;
+    await firebase.firestore().collection("users").doc(uid).set({
+      username,
+      uid,
+      displayName,
+      email,
+      photoURL,
+    });
+    setLoginProcess(LoginState.LoginCompleted);
   }
 
-  function GetUsernameForm() {
+  function getUsernameForm() {
     return (
       <>
         <h2>Selecciona un nombre de usuario</h2>
         <form onSubmit={handleSubmit}>
-          <input onChange={handleChange} />
-          <input type="submit" value="Confirmar" />
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input type="submit" value="Validar" />
+          <span>
+            {loginProcess === LoginState.UsernameAvailable
+              ? "El nombre de usuario está disponible!"
+              : ""}
+            {loginProcess === LoginState.UsernameTaken
+              ? "Escoge otro nombre de usuario, ya existe ese usuario"
+              : ""}
+
+            {loginProcess === LoginState.LoginCompleted ? "Bienvenido" : ""}
+
+            {loginProcess === LoginState.UsernameAvailable ? (
+              <button onClick={handleCompleteProcess}>Terminar registro</button>
+            ) : (
+              ""
+            )}
+          </span>
         </form>
       </>
     );
   }
 
-  function RedirectToComplete() {
-    return <Redirect to="/dashboard" />;
-  }
-
-  return isUserSigned === DataState.Loading ? (
-    <GetUsernameForm />
-  ) : isUserSigned === DataState.Completed ? (
-    <RedirectToComplete />
-  ) : (
-    <Redirect to="/login" />
+  return (
+    <>
+      {getUsernameForm()}
+      {loginProcess === LoginState.LoginCompleted ? (
+        <Redirect to="/dashboard" />
+      ) : (
+        ""
+      )}
+    </>
   );
 }

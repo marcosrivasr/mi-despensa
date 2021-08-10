b8327e111a2f34078d2da0861f1f4175977d8682
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-08-10 01:36:52 -0500

    rediseño de login para mejor flujo

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,73 +1,60 @@
-import { Link } from "react-router-dom";
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-import {
-  FirebaseAuthProvider,
-  FirebaseAuthConsumer,
-  IfFirebaseAuthed,
-  IfFirebaseAuthedAnd,
-} from "@react-firebase/auth";
-import { config } from "../config/config";
 
 import { Redirect } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Login() {
-  const [redirectNickname, setRedirectNickName] = useState(false);
+  const [redirectNickname, setRedirectNickName] = useState(0);
 
   var provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().languageCode = "en";
 
-  useEffect(() => {}, []);
+  async function handleClick() {
+    try {
+      const result = await firebase.auth().signInWithPopup(provider);
 
-  function handleClick() {
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then(async (result) => {
-        /** @type {firebase.auth.OAuthCredential} */
-        var credential = result.credential;
+      /** @type {firebase.auth.OAuthCredential} 
+      var credential = result.credential;*/
 
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        //var token = credential.accessToken;
-        // The signed-in user info.
-        var { uid, email, displayName, photoURL } = result.user;
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      //var token = credential.accessToken;
+      // The signed-in user info.
 
-        const response = await firebase
-          .firestore()
-          .collection("users")
-          .doc(uid)
-          .get();
+      //obtenemos el usuario registrado
+      const response = await firebase
+        .firestore()
+        .collection("users")
+        .doc(result.user.uid)
+        .get();
 
-        if (response.exists) {
-          firebase.firestore().collection("users").doc(uid).set({
-            uid,
-            email,
-            displayName,
-            photoURL,
-          });
-        } else {
-          setRedirectNickName(true);
-        }
-
-        // ...
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
+      if (response.exists) {
+        console.log("El usuario está registrado");
+        setRedirectNickName(1);
+      } else {
+        console.log("El usuario NO está registrado");
+        setRedirectNickName(2);
+      }
+    } catch (error) {
+      // Handle Errors here.
+      console.error(error.code, error.message, error.email, error.credential);
+    }
   }
+
+  function renderUI() {
+    switch (redirectNickname) {
+      case 1:
+        return <Redirect to="/dashboard" />;
+      case 2:
+        return <Redirect to="/nickname" />;
+    }
+  }
+
   return (
     <div>
-      <h2>Login</h2>
-      {redirectNickname ? <Redirect to="/nickname" /> : <br />}
+      <h2>Login {redirectNickname.toString()}</h2>
+      {renderUI()}
       <button onClick={handleClick}>Login con Google</button>
     </div>
   );

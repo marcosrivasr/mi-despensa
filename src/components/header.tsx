7d389d517bdc1862817bb46fe561a3e5b0f8bf7f
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-08-17 22:28:58 -0500

    header updated

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,17 @@
 import { HeaderProps } from "../types/header";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./header.scss";
 
 export default function Header(props: HeaderProps) {
   const { items } = props;
+  const location = useLocation();
   return (
     <div className="header">
       {items.map((item) => (
         <div key={item.id}>
-          <a>
+          <li className={location.pathname === item.goTo ? "active" : ""}>
             <Link to={item.goTo}>{item.title}</Link>
-          </a>
+          </li>
         </div>
       ))}
     </div>

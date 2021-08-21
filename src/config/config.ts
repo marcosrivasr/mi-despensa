ee17d9cbf79468f7febf6f10c7083eb7b7f90cef
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-08-21 18:20:38 -0500

    config file añadido

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,12 +1,10 @@
 export const config = {
-  // DO NOT USE THESE CREDENTIALS ! THEY ARE HERE TO HELP IN THE LEARNING PROCESS.
-  // ANY AND ALL DATA ON THAT DOMAIN IS SUBJECT TO CHANGE AND REMOVAL AT ANY TIME
-  // THIS ACCOUNT IS ALSO ON THE FREE PLAN AND IS SUBJECT TO RESTRICTIONS !
-  databaseURL: "https://mi-despensa-7a229.firebaseio.com",
-  apiKey: "AIzaSyD94iISO20zUX6uMA_xneapYKbgPyQH4y8",
-  authDomain: "mi-despensa-7a229.firebaseapp.com",
-  projectId: "mi-despensa-7a229",
-  storageBucket: "mi-despensa-7a229.appspot.com",
-  messagingSenderId: "800758036960",
-  appId: "1:800758036960:web:11d2eedc6a5fa33fe5d385",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
 };

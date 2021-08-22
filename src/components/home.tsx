dabbc8f7cdf5e62dcda3595b802f847245120691
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-08-22 10:38:09 -0500

    completar compra

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -10,72 +10,108 @@ import {
   existsEntry,
   getShoppingLists,
   getUser,
+  getEntry,
+  getListsInvited,
+  completeItem,
 } from "../services/firestoreService";
 import { IEntry } from "../types/dataState";
 
 export default function Home() {
-  const [today, setToday] = useState([]);
-  const [currentUser, setCurrentUser] = useState(null);
+  const [today, setToday] = useState<IEntry>(null);
+  const [currentUser, setCurrentUser] = useState<firebase.User>(null);
+  let entry: IEntry;
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-        setCurrentUser(user);
-        getUserShoppingListForToday();
+        getUserShoppingListForToday(user);
       } else {
       }
     });
 
-    async function getUserShoppingListForToday() {
-      const currentUser = firebase.auth().currentUser;
-      const entryId = currentUser.uid + "_" + getDate();
+    async function getUserShoppingListForToday(userLogged: firebase.User) {
+      //const currentUser = firebase.auth().currentUser;
+      setCurrentUser(userLogged);
+      const entryId = userLogged.uid + "_" + getDate();
       const exists = await existsEntry(entryId);
-      const user = await getUser(currentUser.uid);
-      const response = await getShoppingLists(user.data().uid);
+      const user = await getUser(userLogged.uid);
+      const response = await getShoppingLists(userLogged.uid);
 
-      const dataToday = [];
+      const todayLists = [];
       //recorre toda la lista
       response.forEach((list) => {
-        const listItem: IListDetails = list.data() as IListDetails;
+        const dataLists: IListDetails = list.data() as IListDetails;
 
-        let todayList = [];
+        let itemsForToday = [];
         //recorre toda la lista de items
-        todayList = listItem.items.filter((item: IItemDetails) => {
+        itemsForToday = dataLists.items.filter((item: IItemDetails) => {
           return getCurrentDayOfTheWeek() === item.day;
         });
 
-        if (todayList.length > 0) {
-          listItem.items = [...todayList];
-          dataToday.push(listItem);
+        if (itemsForToday.length > 0) {
+          dataLists.items = [...itemsForToday];
+          todayLists.push(dataLists);
         }
       });
 
       if (!exists) {
-        const entry: IEntry = {
+        entry = {
           id: entryId,
           date: Date.now(),
-          lists: dataToday,
+          lists: todayLists,
         };
         const res = await createEntry(entry);
-        console.log({ res });
+      } else {
+        entry = await getEntry(entryId);
       }
-      setToday(dataToday);
+      /* try {
+        const invitedLists = await getListsInvited(userLogged.uid);
+        invitedLists.forEach((list) => {
+          //TODO: se obtienen las listas pero se necesita filtrar por aquellas donde ya está el usuario o es el owner
+          entry.lists.push(list.data() as IListDetails);
+        });
+        await createEntry(entry);
+      } catch (error) {} */
+      setToday(entry);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  async function handleCompleted(listId, item: IItemDetails, completed) {
+    const entryId = currentUser.uid + "_" + getDate();
+
+    try {
+      const currentEntry = { ...today };
+      debugger;
+      const indexList = currentEntry.lists.findIndex((x) => x.id === listId);
+      const indexItem = currentEntry.lists[indexList].items.findIndex(
+        (x) => x.productid === item.productid
+      );
+      currentEntry.lists[indexList].items[indexItem].completed = completed;
+      console.log(entryId, listId, item, currentEntry);
+      await completeItem(entryId, currentEntry);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
     <div>
       <h1>Home</h1>
-      {today.length
-        ? today.map((list) => {
+      {currentUser && today && today.lists && today.lists.length
+        ? today.lists.map((list) => {
             return (
               <div>
                 <h2>
-                  {list.title} {list.lists?.length}
+                  {list.title} {list.items.length}
                 </h2>
                 {list.items.map((item: IItemDetails) => {
                   return (
-                    <ShoppingItem item={item} mode="active" list={list.id} />
+                    <ShoppingItem
+                      item={item}
+                      mode="active"
+                      list={list.id}
+                      onChanged={handleCompleted}
+                    />
                   );
                 })}
               </div>

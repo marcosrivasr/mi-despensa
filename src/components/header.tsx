1bb1a92ca975318e9076d6d8b106d1c836c376cb
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-08-22 10:37:56 -0500

    validación de header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,59 @@
 import { HeaderProps } from "../types/header";
 import { Link, useLocation } from "react-router-dom";
 import "./header.scss";
+import firebase from "firebase/app";
+import "firebase/auth";
+import { useEffect, useState } from "react";
 
 export default function Header(props: HeaderProps) {
   const { items } = props;
   const location = useLocation();
+
+  const [currentUser, setCurrentUser] = useState(null);
+
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      setCurrentUser(user);
+    } else {
+      setCurrentUser(null);
+    }
+  });
+
+  function ShowPublicLinks() {
+    return (
+      <>
+        {items
+          .filter((item) => item.protected === false)
+          .map((item) => (
+            <div key={item.id}>
+              <li className={location.pathname === item.goTo ? "active" : ""}>
+                <Link to={item.goTo}>{item.title}</Link>
+              </li>
+            </div>
+          ))}
+      </>
+    );
+  }
+
+  function ShowProtectedLinks() {
+    return (
+      <>
+        {items
+          .filter((item) => item.protected === true)
+          .map((item) => (
+            <div key={item.id}>
+              <li className={location.pathname === item.goTo ? "active" : ""}>
+                <Link to={item.goTo}>{item.title}</Link>
+              </li>
+            </div>
+          ))}
+      </>
+    );
+  }
+
   return (
     <div className="header">
-      {items.map((item) => (
-        <div key={item.id}>
-          <li className={location.pathname === item.goTo ? "active" : ""}>
-            <Link to={item.goTo}>{item.title}</Link>
-          </li>
-        </div>
-      ))}
+      {currentUser ? <ShowProtectedLinks /> : <ShowPublicLinks />}
     </div>
   );
 }
